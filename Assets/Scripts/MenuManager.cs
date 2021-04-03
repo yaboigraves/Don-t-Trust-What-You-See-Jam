5ded27817cfd980971115a5e16b032cd573501ab
Graves <theredhead242@gmail.com>
2021-04-02 18:12:57 -0700

    working on savestate and key binding

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -2,9 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+
 public class MenuManager : MonoBehaviour
 {
-    //ass
+    public Button[] keyBindButtons;
+
+    private void Awake()
+    {
+        SaveStateManager.LoadSaveState();
+        //check and see if the game has launched before, and if not ask for midi binds if the user wants to enter them
+        CheckIfGameLaunched();
+    }
+
+
     public string mainSceneName;
     public void StartGame()
     {
@@ -13,5 +26,78 @@ public class MenuManager : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void CheckIfGameLaunched()
+    {
+        if (SaveStateManager.saveState.hasGameLaunched == 0)
+        {
+            //mark th at we have launched the gamen now (note this doesnt actually save till we call the save function)
+            SaveStateManager.saveState.hasGameLaunched = 1;
+
+            //prompt the user with a ui thing to essentially bind the keys to what they want
+
+        }
+    }
+
+
+    public void CaptureMidiBind(int midi)
+    {
+        //set all the buttons as not interactable 
+        ToggleButtons(false);
+
+        if (midi == 1)
+        {
+
+        }
+        else
+        {
+
+        }
+    }
+
+    public void CaptureKeyBind(int key)
+    {
+        //set all the buttons as not interactable 
+        ToggleButtons(false);
+
+        if (key == 1)
+        {
+            StartCoroutine(captureKeyRoutine());
+        }
+        else
+        {
+            StartCoroutine(captureKeyRoutine());
+        }
+    }
+
+
+
+    IEnumerator captureKeyRoutine()
+    {
+        bool gotKeyInput = false;
+        while (gotKeyInput == false)
+        {
+            //check and see if a key is pressed
+            if (Input.anyKeyDown)
+            {
+                //TODO: 
+                //find the key being pressed
+                string inputString = Input.inputString;
+                Debug.Log("got input string of " + inputString);
+                gotKeyInput = true;
+                ToggleButtons(true);
+            }
+
+            yield return null;
+        }
+    }
+
+    void ToggleButtons(bool toggle)
+    {
+        foreach (Button b in keyBindButtons)
+        {
+            b.interactable = toggle;
+        }
     }
 }
---
+++ Assets/Scripts/SaveStateManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class SaveStateManager
+{
+    //so the only real thing that this needs to do is store what levels have been completed, keybinds, and
+    //whether the game has been launched before
+
+    public static SaveState saveState;
+
+
+    //so this is run when the main scene runs when the game first opens to just load all the settings
+    public static void LoadSaveState()
+    {
+        if (PlayerPrefs.HasKey("hasGameLaunched"))
+        {
+            saveState = new SaveState(PlayerPrefs.GetInt("hasGameLaunched"), PlayerPrefs.GetInt("completedLevels"), PlayerPrefs.GetInt("midiBind1"), PlayerPrefs.GetInt("midiBind2"), PlayerPrefs.GetString("keyBind1"), PlayerPrefs.GetString("keyBind2"));
+
+        }
+        else
+        {
+            ResetSaveState();
+        }
+
+
+    }
+
+    public static void SaveGame()
+    {
+        //basically just set all the player pref from the savestate object that it currently has
+
+        PlayerPrefs.SetInt("hasGameLaunched", saveState.hasGameLaunched);
+        PlayerPrefs.SetInt("completedLevels", saveState.completedLevels);
+        PlayerPrefs.SetInt("midiBind1", saveState.midiBind1);
+        PlayerPrefs.SetInt("midiBind2", saveState.midiBind2);
+        PlayerPrefs.SetString("keyBind1", saveState.keyBind1.ToString());
+        PlayerPrefs.SetString("keyBind2", saveState.keyBind2.ToString());
+
+    }
+
+    public static void ResetSaveState()
+    {
+        PlayerPrefs.DeleteAll();
+        saveState = new SaveState(0, 0, 0, 0, "a", "d");
+        SaveGame();
+    }
+
+}
+
+
+public class SaveState
+{
+    //0 for false, 1 for true
+    public int hasGameLaunched;
+    public int completedLevels;
+    public int midiBind1, midiBind2;
+    public string keyBind1, keyBind2;
+
+    public SaveState(int hasGameLaunched, int completedLevels, int midiBind1, int midiBind2, string keyBind1, string keyBind2)
+    {
+        this.hasGameLaunched = hasGameLaunched;
+        this.completedLevels = completedLevels;
+        this.midiBind1 = midiBind1;
+        this.midiBind2 = midiBind2;
+        this.keyBind1 = keyBind1;
+        this.keyBind2 = keyBind2;
+    }
+}
